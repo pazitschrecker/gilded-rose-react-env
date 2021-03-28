df4ddce8e846a7f9ffc21a9606b43996e0d7649b
Pazit <pazitrose1999@gmail.com>
2021-03-27 20:25:16 -0400

    Added Conjured Item support with unit tests

--- src/api/gilded_rose.spec.js
+++ src/api/gilded_rose.spec.js
@@ -1,9 +1,19 @@
-import {Item, Shop} from "./gilded_rose";
+import { Item, Shop } from "./gilded_rose";
 
-describe("Gilded Rose", function() {
-    it("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
+describe("Conjured Items -- Gilded Rose", function () {
+  it("should reduce quality of conjured item by 2", function () {
+    const gildedRose = new Shop([new Item("Conjured Item", 4, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(3);
+  });
+  it("should reduce quality of conjured item by 1 if too low", function () {
+    const gildedRose = new Shop([new Item("Conjured Item", 4, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+  it("should reduce quality of item by 4 after sellIn", function () {
+    const gildedRose = new Shop([new Item("Conjured Item", -2, 8)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(4);
+  });
 });
